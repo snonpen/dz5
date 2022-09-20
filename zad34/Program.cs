0244f321925191f90657146208165df007ba7c81
snonpen <vinersanya@yandex.ru>
2022-09-20 04:17:34 +0300

    first commit

---
+++ zad34/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Ваш массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int с = 0; с < numbers.Length; с++)
+if (numbers[с] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего в масиве {numbers.Length} чисел(а), {count} из них чётные");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}    
---
+++ zad35/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(numbers);
+int count = 0;
+
+
+for (int c = 0; c < numbers.Length; c++)
+if (numbers[c] > 9)
+if(numbers[c] < 100)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел(а) в массиве, количество из отрезка = {count}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(0,150);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
